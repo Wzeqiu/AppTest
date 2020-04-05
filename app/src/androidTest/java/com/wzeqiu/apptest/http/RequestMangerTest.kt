6096396a9fa8e5e1b347bed20ed313cf22ffd3b4
wzeqiu <wzeqiu@163.com>
2020-04-05 17:58:54 +0800

    jdk version

---
+++ app/src/androidTest/java/com/wzeqiu/apptest/http/RequestMangerTest.kt
@@ -0,0 +1,17 @@
+package com.wzeqiu.apptest.http
+
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class RequestMangerTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
